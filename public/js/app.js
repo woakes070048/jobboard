9c83531608723733913dbee25b4304b1ad9494e8
ormrepo <ormrepo@gmail.com>
2016-05-10 06:55:00 +0000

    added pagination breaks to jobs page

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ Vue.component('job', {
         editJob: function (job) {
             job.editing = true;
         },
-        updateJob: function (job) {
+        updatejob: function (job) {
             this.$http.patch('/api/jobs/' + job.id, job)
             //Set editing to false to show actions again and hide the inputs
             job.editing = false;
@@ -19,9 +19,9 @@ Vue.component('job', {
         storeJob: function (job) {
             this.$http.post('/api/jobs/', job).then(function (response) {
                 /*
-                 After the the new job is stored in the database fetch again all jobs with
-                 vm.fetchJobs();
-                 Or Better, update the id of the created job
+                 After the the new story is stored in the database fetch again all stories with
+                 vm.fetchStories();
+                 Or Better, update the id of the created story
                  */
                 Vue.set(job, 'id', response.data.id);
 
@@ -29,12 +29,8 @@ Vue.component('job', {
                 job.editing = false;
             });
         },
-    }   
-    
-});
-
-
-
+    }
+})
 
 new Vue({
     el: '#v-app',
@@ -45,31 +41,36 @@ new Vue({
         this.fetchJobs()
     },
     methods: {
-        createJob: function() {
-          var newJob = {
-                reference: "",
-                title: "",
-                type: "",
-                description: "",
-                employer: "",
-                location: "",
-                salary: "",
+        createJob: function () {
+            var newJob = {
+                plot: "",
+                upvotes: 0,
                 editing: true
-          };  
+            };
             this.jobs.push(newJob);
         },
-        fetchJobs: function () {
+        fetchJobs: function (page_url) {
             var vm = this;
-            this.$http.get('/api/jobs')
+            page_url = page_url || '/api/jobs'
+            this.$http.get(page_url)
                 .then(function (response) {
-                    // set data on vm
                     var jobsReady = response.data.map(function (job) {
                         job.editing = false;
                         return job
                     })
+                    vm.makePagination(response.data)
                     vm.$set('jobs', jobsReady)
                 });
+        },
+        makePagination(data){
+            //here we use response.data
+            var pagination = {
+                current_page: data.current_page,
+                last_page: data.last_page,
+                next_page_url: data.next_page_url,
+                prev_page_url: data.prev_page_url
             }
+            this.$set('pagination', pagination)
         }
-
-    });
+    }
+});
